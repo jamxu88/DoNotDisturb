8c724d4f7e40881b55590fc3acff74e097f1b2ea
James <43918220+jamxu88@users.noreply.github.com>
2019-01-08 19:20:56 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+  console.log("Bot Ready");
+});
+client.on("message", (message) => {
+  if (message.member.roles.has("527684782587510815")) {
+    message.channel.delete(10)
+    message.author.send("Hey, you're currently on Study Mode. To turn this off, just write `$studymodeoff` in the general chat. Thank you for being a good student!")
+  }else
+  if (message.mentions.users.first()) {
+    if (message.mentions.users.find("527684782587510815")) {
+      message.channel.delete(10)
+      message.author.send("Hey, this person is currently in Study Mode.")
+      }
+  }else
+  if (message.content.startsWith("$studdymodeoff")) {
+    let role = message.guild.roles.find("527684782587510815")
+    let member = message.author.id
+    member.removeRole("527684782587510815").catch(console.error)
+  }else
+  if (message.content.startsWith("$studymodeon")) {
+    let role = message.guild.roles.find("527684782587510815")
+    let member = message.author.id
+    member.addRole("527684782587510815").catch(console.error)
+  }
+  });
+client.login(process.env.BOT_TOKEN)
