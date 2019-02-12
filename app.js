51a5f39438acb63a5ad5d7283af73e2f8e02f13e
James <43918220+jamxu88@users.noreply.github.com>
2019-02-11 21:36:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
-  console.log("Bot Ready");
+  console.log("DND Bot On!");
 });
 client.on("message", (message) => {
-  if (message.member.roles.some(r=>["DND"].includes(r.name))) {
-    message.delete(10)
-    message.author.send("Hey, you're currently on Study Mode. To turn this off, just write `$studymodeoff` in the general chat. Thank you for being a good student!")
-  }else
-  if (message.mentions.users.first()) {
-    if (message.mentions.users.find("527684782587510815")) {
-      message.delete(10)
-      message.author.send("Hey, this person is currently in Study Mode.")
-      }
-  }else
   if (message.content.startsWith("$studdymodeoff")) {
-    let role = message.guild.roles.find("527684782587510815")
+    let role = message.guild.roles.find("527684778653253642")
     let member = message.author.id
-    member.removeRole("527684782587510815").catch(console.error)
+    member.removeRole("527684778653253642").catch(console.error)
   }else
   if (message.content.startsWith("$studymodeon")) {
-    let role = message.guild.roles.find("527684782587510815")
+    let role = message.guild.roles.find("527684778653253642")
     let member = message.author.id
-    member.addRole("527684782587510815").catch(console.error)
+    member.addRole("527684778653253642").catch(console.error)
   }
   });
 client.login(process.env.BOT_TOKEN)
