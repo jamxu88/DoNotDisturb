9cf280422345c96f86549080c28774a655255b7c
James <43918220+jamxu88@users.noreply.github.com>
2019-02-12 17:52:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ client.on("message", (message) => {
   if (message.content.startsWith("!dndoff")) {
     let role = message.guild.roles.find(role => role.name === "NoDisturb");
     let member = message.member
-    member.roles.remove(role).catch(console.error)
+    member.removeRole(role).catch(console.error)
   }else
   if (message.content.startsWith("!dndon")) {
     let role = message.guild.roles.find(role => role.name === "NoDisturb");
     let member = message.member
-    member.roles.add(role).catch(console.error)
+    member.addRole(role).catch(console.error)
   }
   });
 client.login(process.env.BOT_TOKEN)
