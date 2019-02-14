9d67a9afd30242535ea33c5dd9a6e2c96d2a6d57
James <43918220+jamxu88@users.noreply.github.com>
2019-02-13 21:41:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("DND Bot On!");
 });
-client.on('messageReactionRemove', (reaction, user, message) => {
-  if(reaction.message.id === "545430535871266817") {
-    let role = reaction.guild.roles.find(role => role.name === "NoDisturb");
-    let member = reaction.member
+client.on("message", (message) => {
+  if (message.content.startsWith("!dndoff")) {
+    let role = message.guild.roles.find(role => role.name === "NoDisturb");
+    let member = message.member
     member.removeRole(role).catch(console.error)
-  }
-});
-client.on('messageReactionAdd', (reaction, user, message) => {
-  if (reaction.message.id === "545430535871266817") {
+  }else
+  if (message.content.startsWith("!dndon")) {
     let role = message.guild.roles.find(role => role.name === "NoDisturb");
     let member = message.member
     member.addRole(role).catch(console.error)
   }
-});
+  });
 client.login(process.env.BOT_TOKEN)
