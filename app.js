8d6938e0260bb280638f6a384e652fc415b6d050
James <43918220+jamxu88@users.noreply.github.com>
2019-02-20 20:54:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.on("message", (message) => {
   if (message.content.startsWith("!dndon")) {
     let role = message.guild.roles.find(role => role.name === "NoDisturb");
     let member = message.member
-    member.addRole(role).catch(console.error)
+    member.addRole(role).catch(console.error);
   }else
   if (message.channel.id === "545076810036281354") {
     message.delete();
@@ -21,7 +21,8 @@ client.on("message", (message) => {
   if (message.content.startsWith("i need to work")) {
     let role = message.guild.roles.find(role => role.name === "NoDisturb");
     let member = message.member
-    member.addRole(role).catch(console.error)
+    member.addRole(role).catch(console.error);
+    message.delete();
   }
 });
 client.login(process.env.BOT_TOKEN)
